863d0f2fb5a81e3a509ffd4baf98d53b911ee597
syirnik._. <Nadija.kirillova@yandex.ru>
2024-04-21 08:46:05 +0300

    Comment to commit

---
+++ Project_for_21/middlewares/cors.js
@@ -0,0 +1,14 @@
+function cors(req, res, next) {
+    const { origin } = req.headers;
+    
+    if (allowedCors.includes(origin)) { // Если это наш друг
+        res.header('Access-Control-Allow-Origin', origin);
+    }
+    
+    next();
+}
+const allowedCors = [
+    'https://practicum.yandex.ru',
+    'https://students-projects.ru',
+    'localhost:3000'
+  ];
